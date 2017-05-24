7bbc1b3641b055c258db09d0a4258ef2d3b04c99
vamsi <vamsi@Nagendras-Mac-mini.local>
2017-05-24 13:33:35 +0530

    Initial Commit

---
+++ Sirisha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sirisha
+//
+//  Created by vamsi on 24/05/17.
+//  Copyright © 2017 vamsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sirisha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sirisha
+//
+//  Created by vamsi on 24/05/17.
+//  Copyright © 2017 vamsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
